1f1db1587324a6a3d9f95ca4add611e1f5006c26
ddelfaus <ddelfaus@gmail.com>
2019-12-02 15:26:59 -0500

    first commit and get users request

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // implement your API here
+const express = require('express');
+
+// getting data
+
+const db = require('./data/db')
+
+
+
+const server = express();
+
+
+server.get('/', (req, res) =>{
+    res.send({ api: "up and running.."})
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+
+    .then(users => {
+        res.status(200).json(users)
+    })
+
+    .catch( error => {
+        console.log('error on GET /hubs', error);
+        res.status(500).json({ errorMessage: "error getting list of hubs from database"})
+
+    })
+})
+
+
+const port = 4000
+server.listen(port, () => console.log(`\n API running on port ${port}##\n`))
